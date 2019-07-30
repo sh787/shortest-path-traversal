3c61b29e1b710bd916ffc68ee5707189d8e04bc7
Sophia <Sophia@172.20.12.204>
2019-07-30 14:32:01 -0400

    Pair program w Julia Chang: add contains and getPriority to Group 5 Heap

--- a5-release/src/a5/Heap.java
+++ a5-release/src/a5/Heap.java
@@ -32,6 +32,25 @@ public class Heap<E, P> implements PriorityQueue<E, P>{
 		priorityMap = new HashMap<Integer, P>();
 	}
 	
+	/**
+	 * Added this method to Group 5's Heap implementation. 
+	 * Returns whether the Heap contains Element e.
+	 */
+	public boolean contains(E e) {
+		return locationMap.containsKey(e);
+	}
+	
+	/**
+	 * Added this method to Group 5's Heap implementation. 
+	 * Returns the Priority of Element e.
+	 */
+	public P getPriority (E e) {
+		if(!(this.data.contains(e)))
+			throw new NoSuchElementException("Heap does not contain e.");
+		return priorityMap.get(locationMap.get(e));
+	}
+	
+	
 	@Override
 	/**
 	 * @return the comparator of the PriorityHeap
@@ -226,158 +245,4 @@ public class Heap<E, P> implements PriorityQueue<E, P>{
 		else
 			return -1;
 	}
-
-public static class Tests {
-		
-		public static void assertInvariants(Heap<?, Integer> heap) throws AssertionFailedError {
-			if(heap.size() > 0) {
-				recurTestLeftRightInvariant(heap, 0);
-			}
-		}
-		
-		/** recursively tests whether the left and right elements(if exists) of a index is smaller than or equal to their parent */
-		private static void recurTestLeftRightInvariant(Heap<?, Integer> heap, int index) throws AssertionFailedError {
-			if(index < 0 || index >= heap.size())
-				return;
-			
-			int left = heap.left(index);
-			int right = heap.right(index);
-			//the priority left, right elements of the element must be either nonexistent or smaller than/the same to the parent
-			if((left != -1 && heap.comparator.compare(heap.priorityMap.get(index), heap.priorityMap.get(left)) < 0)
-					||(right != -1 && heap.comparator.compare(heap.priorityMap.get(index), heap.priorityMap.get(right)) < 0)) {
-				throw new AssertionFailedError(String.format("LeftRightInvariant failed at index %d, depth %d (this:%s left:%s right:%s)",
-						index, 
-						heap.depth(index), 
-						heap.priorityMap.get(index).toString(),
-						heap.priorityMap.get(left).toString(),
-						heap.priorityMap.get(right).toString()
-						));
-			}
-			recurTestLeftRightInvariant(heap, left);
-			recurTestLeftRightInvariant(heap, right);
-		}
-		
-		/** create a heap with the input arguments of increasing priority*/
-		Heap<String, Integer> makeHeap(String ...strs) {
-			Heap<String, Integer> heap = new Heap<String, Integer>(Comparator.naturalOrder());
-			int prior = 0;
-			for(String str : strs) {
-				heap.add(str, prior--);
-			}
-			return heap;
-		}
-
-		@Test
-		void testPeek() {
-			Heap<String, Integer> heap = makeHeap("test", "asda", "kkk");
-			assertEquals("test", heap.peek());
-		}
-		
-		@Test
-		void testPoll() {
-			Heap<String, Integer> heap = makeHeap("test", "asda", "kkk");
-			assertEquals("test", heap.poll());
-			System.out.println();
-			assertEquals("asda", heap.poll());
-			System.out.println();
-			assertEquals("kkk", heap.poll());
-			Heap<String, Integer> heap2 = makeHeap();
-			Heap<String, Integer> heap3 = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			assertThrows(NoSuchElementException.class,()->{heap.poll();});
-			assertEquals("1", heap3.poll());
-			while(heap3.poll() != "10") {
-				heap3.poll();
-			}
-			assertEquals("11", heap3.poll());
-		}
-		
-		
-		@Test
-		void testAdd() {
-			Heap<String, Integer> heap = makeHeap("1", "2", "3", "4", "5", "6");
-			assertInvariants(heap);
-			Heap<String, Integer> heap2 = new Heap<String, Integer>(Comparator.naturalOrder());
-			heap.add("9", 10);
-			assertEquals((Integer)10,heap.priorityMap.get(0));
-			
-			assertThrows(IllegalArgumentException.class,()->{heap.add("1", 1);});
-		
-		}
-		
-		@Test
-		void testChangePriority() {
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6");
-			heap.changePriority("2", 3);
-			heap.changePriority("3", 8);
-			heap.changePriority("6", -9);
-			assertEquals((Integer)3,heap.priorityMap.get(1));
-			assertEquals((Integer)8,heap.priorityMap.get(2));
-			assertEquals((Integer)(-4), heap.priorityMap.get(4));
-			assertEquals((Integer)(-9), heap.priorityMap.get(5));
-		
-		}
-		
-		@Test
-		void testLeft() {
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			assertEquals(1, heap.left(0));
-			assertEquals(7,heap.left(3));
-			assertEquals(11,heap.left((heap.left(2))));
-		}
-		
-		@Test
-		void testRight(){
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			assertEquals(2,heap.right(0));
-			assertEquals(8,heap.right(3));
-			assertEquals(10,heap.right((heap.right(1))));
-		}
-		
-		@Test
-		void testParent() {
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			assertEquals(0,heap.parent(2));
-			assertEquals(4,heap.parent(9));
-			assertEquals(5,heap.parent(11));
-			assertEquals(1,heap.parent(4));
-			assertEquals(0,heap.parent(heap.parent(4)));
-		}
-		
-		@Test
-		void testSwap() {
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			heap.swap(0, 1);
-			heap.swap(9, 2);
-			heap.swap(8, 1);
-			assertEquals((Integer)0,heap.locationMap.get("2"));
-			assertEquals((Integer)2,heap.locationMap.get("10"));
-			assertEquals((Integer)8,heap.locationMap.get("1"));
-			assertEquals((Integer)(-2),heap.priorityMap.get(9));
-			assertEquals((Integer)(-9),heap.priorityMap.get(2));
-			assertEquals((Integer)(-4), heap.priorityMap.get(4));
-			assertEquals((Integer)(-8), heap.priorityMap.get(1));
-			assertEquals((Integer)0, heap.priorityMap.get(8));	
-			assertEquals((Integer)(-1),heap.priorityMap.get(0));
-		}
-		
-		@Test
-		void testNoOfElements() {
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			assertEquals(4,heap.noOfElements(5));
-			assertEquals(8,heap.noOfElements(11));
-			assertEquals(2,heap.noOfElements(1));
-			assertEquals(1,heap.noOfElements(0));
-		}
-		
-		@Test
-		void testDepth() {
-			Heap<String, Integer> heap = makeHeap("1","2","3","4","5","6","7","8","9","10","11","12");
-			assertEquals(4,heap.depth(11));
-			assertEquals(4,heap.depth(7));
-			assertEquals(2,heap.depth(1));
-			assertEquals(1,heap.depth(0));
-			assertEquals(3,heap.depth(5));
-		}
-	}
-
 }
